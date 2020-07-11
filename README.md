7ba394d20577198afe8de5bcd2891d214b1fcdd3
kboysm <lishal12@naver.com>
2020-07-11 18:39:02 +0900

    2차 포트폴리오 화면 테스트

--- README.md
+++
@@ -1,4 +0,0 @@
-# kboysm.github.io
-신입 웹 개발자 포트폴리오
-
-Vue.js로 제작 -> yarn build -> dist 폴더 push
---
+++ main.js
@@ -0,0 +1,37 @@
+(function () {
+
+    let controller = new ScrollMagic.Controller();
+    let sectionOneOpacity = document.querySelector('.one');
+
+    let wipeAnimation = new TimelineMax()
+        // animate to second
+        .to("#slideContainer", 1, { z: -500, opacity: 0, ease: Linear.ease })
+        .to("#slideContainer", 1, { x: "-25%" })
+        .to("#slideContainer", 1, { z: 0, opacity: 1, ease: Linear.ease })
+        // animate to third
+        .to("#slideContainer", 1, { z: -500, delay: 0.6, opacity: 0, ease: Linear.ease })
+        .to("#slideContainer", 1, { x: "-50%" })
+        .to("#slideContainer", 1, { z: 0, opacity: 1, ease: Linear.ease })
+        // animate to forth
+        .to("#slideContainer", 1, { z: -500, delay: 0.6, opacity: 0, ease: Linear.ease })
+        .to("#slideContainer", 1, { x: "-75%" })
+        .to("#slideContainer", 1, { z: 0, opacity: 1, ease: Linear.ease })
+
+
+    let scene = new ScrollMagic.Scene({
+        triggerElement: "#pinContainer",
+        triggerHook: "onLeave",
+        duration: "200%" //이 값이 클 수록 천천히 덮어씀
+    }).setTween(".one", 1, { opacity: 0 }) // trigger a TweenMax.to tween
+        .setPin("#pinContainer")
+        .setTween(wipeAnimation)
+        .addIndicators()
+        .addTo(controller);
+
+    setTimeout(() => {
+        sectionOneOpacity.style.opacity = 1;
+        sectionOneOpacity.style.transition = '.6s';
+    }, 1000)
+
+
+}())
