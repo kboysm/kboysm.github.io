ea27d0d5c98da43343378fcaa48dd1057506cb31
kboysm <lishal12@naver.com>
2020-07-20 19:01:52 +0900

    main.js height 추가

--- main.js
+++ main.js
@@ -7,22 +7,25 @@
         // animate to second
         .to("#slideContainer", 1, { z: -500, opacity: 0, ease: Linear.ease })
         .to("#slideContainer", 1, { x: "-25%" })
-        .to("#slideContainer", 1, { z: 0, opacity: 1, ease: Linear.ease })
+        .to("#slideContainer", 1, { z: 0, opacity: 1, ease: Linear.ease, height: "100%" })
         // animate to third
         .to("#slideContainer", 1, { z: -500, delay: 0.6, opacity: 0, ease: Linear.ease })
         .to("#slideContainer", 1, { x: "-50%" })
-        .to("#slideContainer", 1, { z: 0, opacity: 1, ease: Linear.ease })
+        .to("#slideContainer", 1, { z: 0, opacity: 1, ease: Linear.ease, height: "100%" })
         // animate to forth
         .to("#slideContainer", 1, { z: -500, delay: 0.6, opacity: 0, ease: Linear.ease })
         .to("#slideContainer", 1, { x: "-75%" })
-        .to("#slideContainer", 1, { z: 0, opacity: 1, ease: Linear.ease })
+        .to("#slideContainer", 1, { z: 0, opacity: 1, ease: Linear.ease, height: "100%" })
 
 
     let scene = new ScrollMagic.Scene({
         triggerElement: "#pinContainer",
         triggerHook: 0,
         duration: "200%" //이 값이 클 수록 천천히 덮어씀
-    }).setTween(".one", 1, { opacity: 0 }) // trigger a TweenMax.to tween
+    }).setTween(".one", 1, { opacity: 0, height: "100%" }) // trigger a TweenMax.to tween
+        .setTween(".two", 1, { height: "100%" })
+        .setTween(".three", 1, { height: "100%" })
+        .setTween(".four", 1, { height: "100%" })
         .setPin("#pinContainer")
         .setTween(wipeAnimation)
         .addIndicators()
